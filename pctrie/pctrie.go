edb23caf7dba7ffa2b33ec527e628958c7a4b9e7
Parham Alvani <parham.alvani@gmail.com>
2017-12-22 00:14:49 +0330

    Adds basic of pc-trie implementation.

--- pctrie/pctrie.go
+++ pctrie/pctrie.go
@@ -16,14 +16,33 @@ import (
 
 // PCTrie represents prefix-compresed trie data structure
 type PCTrie struct {
+	Bitmap   int
+	NextHops [][]string
+	Size     int
 }
 
 // New creates new prefix-compresed trie
-func New(t *trie.Trie, compSize int) {
+func New(t *trie.Trie, compSize int) *PCTrie {
 	nodes := t.ToArray()
+	bitmap := 0
+	i := 0
 
-	for s := compSize - 1; s < len(nodes); s += compSize {
-		for t := s; t < s+compSize-1; t++ {
+	for s := compSize; s < len(nodes)-1; s += compSize {
+		empty := true
+		for t := s; t < s+compSize; t++ {
+			if nodes[t].NextHop != "" {
+				empty = false
+			}
 		}
+		if !empty {
+			bitmap |= 1 << uint(i)
+		} else {
+		}
+		i++
+	}
+
+	return &PCTrie{
+		Bitmap: bitmap,
+		Size:   i,
 	}
 }
