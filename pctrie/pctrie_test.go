17cdea8368237e58829af87e00c3814cebdc46f9
Parham Alvani <parham.alvani@gmail.com>
2017-12-22 01:04:59 +0330

    Better pc-trie test cases.

---
+++ pctrie/pctrie_test.go
@@ -0,0 +1,50 @@
+/*
+ * +===============================================
+ * | Author:        Parham Alvani <parham.alvani@gmail.com>
+ * |
+ * | Creation Date: 21-12-2017
+ * |
+ * | File Name:     pctrie_test.go
+ * +===============================================
+ */
+
+package pctrie
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/1995parham/FlashTrie.go/trie"
+)
+
+func TestBasic1(t *testing.T) {
+	trie := trie.New()
+
+	trie.Add("*", "A")
+	trie.Add("1*", "B")
+	trie.Add("00*", "C")
+	trie.Add("11*", "D")
+	trie.Add("100*", "E")
+
+	pctrie := New(trie, 2)
+	bitmap := fmt.Sprintf("%0*b", pctrie.Size, pctrie.Bitmap)
+	if bitmap != "0100110" {
+		t.Fatal("Invalid bitmap")
+	}
+}
+
+func TestBasic2(t *testing.T) {
+	trie := trie.New()
+
+	trie.Add("*", "A")
+	trie.Add("1*", "B")
+	trie.Add("00*", "C")
+	trie.Add("11*", "D")
+	trie.Add("100*", "E")
+
+	pctrie := New(trie, 4)
+	bitmap := fmt.Sprintf("%0*b", pctrie.Size, pctrie.Bitmap)
+	if bitmap != "101" {
+		t.Fatal("Invalid bitmap")
+	}
+}
