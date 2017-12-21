eec4206955942309300e28117b8dfb4f550780e9
Parham Alvani <parham.alvani@gmail.com>
2017-12-22 01:04:45 +0330

    Completes pc-trie.

--- pctrie/pctrie.go
+++ pctrie/pctrie.go
@@ -11,6 +11,8 @@
 package pctrie
 
 import (
+	"fmt"
+
 	"github.com/1995parham/FlashTrie.go/trie"
 )
 
@@ -22,27 +24,43 @@ type PCTrie struct {
 }
 
 // New creates new prefix-compresed trie
-func New(t *trie.Trie, compSize int) *PCTrie {
-	nodes := t.ToArray()
+func New(tr *trie.Trie, compSize int) *PCTrie {
+	nodes := tr.ToArray()
+	nhs := make([][]string, 0)
 	bitmap := 0
-	i := 0
+	size := 0
 
 	for s := compSize; s < len(nodes)-1; s += compSize {
 		empty := true
+		nh := make([]string, compSize)
+
 		for t := s; t < s+compSize; t++ {
+			nh[t-s] = tr.Lookup(fmt.Sprintf("%b", t)[1:] + "*")
 			if nodes[t].NextHop != "" {
 				empty = false
 			}
 		}
 		if !empty {
-			bitmap |= 1 << uint(i)
+			bitmap |= 1 << uint(size)
+			nhs = append(nhs, nh)
 		} else {
+			nhs = append(nhs, make([]string, 0))
 		}
-		i++
+		size++
+	}
+
+	for i := 0; i <= size/2; i++ {
+		if bitmap&(1<<uint(i)) != 0 {
+			if bitmap&(1<<uint(2*i+1)) != 0 && bitmap&(1<<uint(2*i+2)) != 0 {
+				bitmap ^= 1 << uint(i)
+				nhs[i] = make([]string, 0)
+			}
+		}
 	}
 
 	return &PCTrie{
-		Bitmap: bitmap,
-		Size:   i,
+		Bitmap:   bitmap,
+		Size:     size,
+		NextHops: nhs,
 	}
 }
