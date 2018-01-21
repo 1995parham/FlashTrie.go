000b23975e5b68a7381033d00ef89711325c8504
Parham Alvani <parham.alvani@gmail.com>
2018-01-22 02:25:56 +0330

    Improves main tests.

--- main_test.go
+++ main_test.go
@@ -14,6 +14,7 @@ import (
 
 	"github.com/AUTProjects/FlashTrie.go/pctrie"
 	"github.com/AUTProjects/FlashTrie.go/trie"
+	"github.com/AUTProjects/FlashTrie.go/util"
 )
 
 var ipAddress = []string{
@@ -25,9 +26,9 @@ var ipAddress = []string{
 }
 
 func TestBasic(t *testing.T) {
-	r1 := parseCIDR("192.0.2.1/4")
-	r2 := parseCIDR("192.0.2.1/8")
-	r3 := parseCIDR("172.0.2.1/8")
+	r1, _ := util.ParseNet("192.0.2.1/4")
+	r2, _ := util.ParseNet("192.0.2.1/8")
+	r3, _ := util.ParseNet("172.0.2.1/8")
 
 	trie := trie.New()
 	trie.Add(r1, "A")
@@ -37,9 +38,9 @@ func TestBasic(t *testing.T) {
 	pctrie := pctrie.New(trie, 4)
 
 	for _, ip := range ipAddress {
-		if trie.Lookup(parseIP(ip)) != pctrie.Lookup(parseIP(ip)) {
-			t.Fatalf("Invalid route %s: %s != %s", ip, trie.Lookup(parseIP(ip)), pctrie.Lookup(parseIP(ip)))
+		if trie.Lookup(util.ParseIP(ip)) != pctrie.Lookup(util.ParseIP(ip)) {
+			t.Fatalf("Invalid route %s: %s != %s", ip, trie.Lookup(util.ParseIP(ip)), pctrie.Lookup(util.ParseIP(ip)))
 		}
-		t.Logf("%s: %s", ip, trie.Lookup(parseIP(ip)))
+		t.Logf("%s: %s", ip, trie.Lookup(util.ParseIP(ip)))
 	}
 }
