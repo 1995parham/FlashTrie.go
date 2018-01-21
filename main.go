f7e892aa3c5ffa24b2f328d2fe01386ffcfee675
Parham Alvani <parham.alvani@gmail.com>
2018-01-22 02:23:42 +0330

    Removes utility functions from main.

--- main.go
+++ main.go
@@ -10,40 +10,5 @@
 
 package main
 
-import (
-	"fmt"
-	"log"
-	"net"
-)
-
-func parseCIDR(cidr string) string {
-	_, ipv4Net, err := net.ParseCIDR(cidr)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	size, _ := ipv4Net.Mask.Size()
-	str := ""
-
-	for _, octet := range ipv4Net.IP.To4() {
-		str = str + fmt.Sprintf("%08b", octet)
-	}
-	str = str[:size] + "*"
-
-	return str
-}
-
-func parseIP(ip string) string {
-	ipv4 := net.ParseIP(ip)
-
-	str := ""
-
-	for _, octet := range ipv4.To4() {
-		str = str + fmt.Sprintf("%08b", octet)
-	}
-
-	return str
-}
-
 func main() {
 }
