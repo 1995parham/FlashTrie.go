967e532d5ff42407e257278f476e034cb4cf45a7
Parham Alvani <parham.alvani@gmail.com>
2018-02-01 09:39:02 +0330

    Adds shell :dancer: :tada:.

--- main.go
+++ main.go
@@ -10,5 +10,75 @@
 
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/AUTProjects/FlashTrie.go/fltrie"
+	"github.com/AUTProjects/FlashTrie.go/util"
+	"github.com/abiosoft/ishell"
+	yaml "gopkg.in/yaml.v2"
+)
+
+type route struct {
+	Route   string
+	Nexthop string
+}
+
 func main() {
+	var f string
+	var routes []route
+
+	fmt.Print("Routes file name (without .yml extension): ")
+	fmt.Scanf("%s", &f)
+	f += ".yml"
+
+	data, err := ioutil.ReadFile(f)
+	if err != nil {
+		log.Fatalf("Reading file %s failed with: %s\n", f, err)
+		os.Exit(1)
+	}
+
+	err = yaml.Unmarshal(data, &routes)
+	if err != nil {
+		log.Fatalf("Parsing file %s failed with: %s\n", f, err)
+	}
+
+	// Building flash trie
+
+	fltrie := fltrie.New()
+
+	for _, route := range routes {
+		r, err := util.ParseNet(route.Route)
+		if err != nil {
+			log.Fatalf("Parsing file %s failed with: %s\n", f, err)
+			os.Exit(1)
+		}
+		fltrie.Add(r, route.Nexthop)
+	}
+
+	if err := fltrie.Build(); err != nil {
+		log.Fatalf("Building flash trie failed with: %s\n", err)
+		os.Exit(1)
+	}
+
+	// Run shell
+
+	shell := ishell.New()
+	shell.Println("Welcome to FlashTrie.go by Parham Alvani @ 2018")
+
+	shell.AddCmd(&ishell.Cmd{
+		Name: "lookup",
+		Help: "lookup destination in routing table",
+		Func: func(c *ishell.Context) {
+			for _, arg := range c.Args {
+				ip := util.ParseIP(arg)
+				c.Printf("%s -> %s\n", arg, fltrie.Lookup(ip))
+			}
+		},
+	})
+
+	shell.Run()
 }
